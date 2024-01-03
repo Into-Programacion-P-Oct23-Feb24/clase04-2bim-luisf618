8f4d7cd939bde8b08da54bace672fe5e401e1d50
Luis Cordova <lcordovacarrino@gmail.com>
2024-01-03 16:44:24 -0500

    Funciones y procedimientos

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -13,16 +13,26 @@ public class Ejemplo041 {
 
     public static void main(String[] args) {
         // 
-        obtenerMultiplicacion(primerValor, segundoValor); 
-        
+        int resultado;
+        String cadena;
+        int[][] valor1 = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
+        int[][] valor2 = {{1, 2, 3}, {2, 2, 2}, {3, 1, 2}};
+        for (int f = 0; f < valor1.length; f++) {
+            cadena = "";
+            for (int c = 0; c < valor1[f].length; c++) {
+                resultado = obtenerMultiplicacion(valor1[f][c], valor2[f][c]);
+                cadena = String.format("%s%s\t",
+                        cadena, resultado);
+            }
+            System.out.printf("%s\n", cadena);
+        }
+
     }
-        
-    public static void obtenerSuma(int a, int b){
-        int suma;
-        suma = a + b;
-        System.out.printf("El valor de la suma es: %d\n", suma);
-        
+
+    public static int obtenerMultiplicacion(int a, int b) {
+        int mult;
+        mult = a * b;
+        return mult;
     }
-    
-    
+
 }
