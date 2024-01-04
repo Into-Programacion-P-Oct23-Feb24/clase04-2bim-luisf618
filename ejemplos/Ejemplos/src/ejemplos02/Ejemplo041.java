c7784f9365d7ccd0de89872861ae0fb5e24425ba
Luis Cordova <lcordovacarrion@gmail.com>
2024-01-04 00:37:37 -0500

    Funciones y procedimientos

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -20,20 +20,30 @@ public class Ejemplo041 {
         int[][] valor1 = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
         int[][] valor2 = {{1, 2, 3}, {2, 2, 2}, {3, 1, 2}};
         for (int f = 0; f < valor1.length; f++) {
-            cadena = "";
             cadena1 = "";
+            for (int c = 0; c < valor1[f].length; c++) {
+                cadena1 = String.format("%s%s\t",
+                        cadena1, valor1[f][c]);
+            }
+            System.out.printf("%s\n", cadena1);
+        }
+        System.out.printf("\n");
+        for (int f = 0; f < valor1.length; f++) {
             cadena2 = "";
+            for (int c = 0; c < valor1[f].length; c++) {
+                cadena2 = String.format("%s%s\t",
+                        cadena2, valor2[f][c]);
+            }
+            System.out.printf("%s\n", cadena2);
+        }
+        System.out.printf("\n");
+        for (int f = 0; f < valor1.length; f++) {
+            cadena = "";
             for (int c = 0; c < valor1[f].length; c++) {
                 resultado = obtenerMultiplicacion(valor1[f][c], valor2[f][c]);
                 cadena = String.format("%s%s\t",
                         cadena, resultado);
-                // cadena1 = String.format("%s%s\t",
-                //        cadena1, valor1[f][c]);
-                // cadena2 = String.format("%s%s\t",
-                //        cadena2, valor2[f][c]);
             }
-            // System.out.printf("%s\n",cadena1);
-            // System.out.printf("\n%s\n",cadena2);
             System.out.printf("%s\n", cadena);
         }
 
@@ -44,5 +54,4 @@ public class Ejemplo041 {
         mult = a * b;
         return mult;
     }
-
 }
